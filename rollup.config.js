b50411220cb0b1622258c9303ae68490d6a036f3
ohryan <ryanachacon@gmail.com>
2020-08-05 09:14:41 -0600

    update rollup config to add plugins for bundling correctly

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,35 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
 import pkg from './package.json'
 
-export default {
-  input: 'lib/index.js',
-  output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' }
-  ]
-}
+export default [
+  // browser-friendly UMD build
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'lib/index.js',
+    // external: ['ms'], what are externals?
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+      {
+        name: 'ol6-react-umd',
+        file: pkg.browser,
+        format: 'umd'
+      }
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      postcss()
+    ]
+  }
+]
